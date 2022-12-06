4db4e9fb0a1307c202ffa25274b01b31950354b4
aprew <prowott@gmail.com>
2022-12-06 16:51:21 +0000

    solution six

---
+++ src/six.java
@@ -0,0 +1,52 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.LinkedList;
+
+public class six {
+    public static void main(String[] args) {
+        try {
+            BufferedReader bufferedReader = new BufferedReader(new FileReader("./six.txt"));
+            String line;
+            HashMap<Character, Integer> charCount = new HashMap<>();
+            LinkedList<Character> charHistory = new LinkedList<>();
+            final int MARKER = 14;
+            while ((line = bufferedReader.readLine()) != null) {
+                int count = 0;
+                charCount.clear();
+                charHistory.clear();
+                for(char c : line.toCharArray()) {
+                    count++;
+                    charHistory.add(c);
+                    if(!charCount.containsKey(c)) {
+                        charCount.put(c,1);
+                    } else {
+                        charCount.replace(c,charCount.get(c)+1);
+                    }
+                    if(charCount.size()==MARKER) {
+                        int sum=0;
+                        for(Integer i : charCount.values()) {
+                            sum+=i;
+                        }
+                        if(sum==MARKER) {
+                            System.out.println(count);
+                            break;
+                        }
+                    }
+                    if(charHistory.size()==MARKER) {
+                        char charToRemove = charHistory.poll();
+                        if(charCount.get(charToRemove) > 1) {
+                            charCount.replace(charToRemove,charCount.get(charToRemove)-1);
+                        } else {
+                            charCount.remove(charToRemove);
+                        }
+                    }
+                }
+            }
+            bufferedReader.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
